14771b2238607d84033571065817dccd7f4f56aa
Leonardo Barbosa <leoopzem@gmail.com>
2026-09-09 15:20:17 -0300

    Somativa

--- 2-Termo/BACKEND [JAVASCRIPT]/Somativa/Primeira_Somativa/ex05_ciclos.js
+++ 2-Termo/BACKEND [JAVASCRIPT]/Somativa/Primeira_Somativa/ex05_ciclos.js
@@ -1,8 +1,9 @@
 const Input = require('readline-sync');
 
-const Quantidade_Pecas = Input.questionInt("Digite a quantidade de pecas por ciclo:  ");
+const Quantidade_Pecas = Input.questionInt("Digite a quantidade de pecas por ciclo: ");
 
-for (let i = 0; i <=10; i++) {
-    let Calculo = Quantidade_Pecas + i++;
-    console.log(`Ciclo: ${i+1} = ${Calculo} `);
+for (let i = 0; i <= 10; i++) {
+    let Calculo = Quantidade_Pecas + i; 
+    
+    console.log(`Ciclo: ${i + 1} = ${Calculo}`);
 }
